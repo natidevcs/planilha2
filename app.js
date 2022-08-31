b11c6e2de84708f24d6dda652e95c061c8e6399a
Natália Pereira Smaniotto <natiazevedop@gmail.com>
2022-08-31 23:51:19 +0000

    atualizações app.js

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 require('dotenv/config')
-const express = require ('express')
+const express = require('express')
 
-const TransacoesRepositorio = require("./infra/sql-transacoes-repositorio.js")
+const TransacoesRepositorio = require("./infra/sql-transacoes-repositorio")
 
 const app = express()
-const port = 3000;
 
-// precisa ter para acessar o req.body
-app.use(express.json())
- // "serve" arquivos da pasta public
+const port = process.env.PORT || 3000
+
+function mostraReq(req) {
+    console.log(`${req.method} ${req.url} ${JSON.stringify(req.body)}`)
+}
+
+// Permite acessar o req.body
+app.use(express.json());
+// "Serve" arquivos da pasta public
 app.use(express.static(`${__dirname}/public`))
 
-app.get('/transacoes', async (req, res) => { 
-  const repositorio = new TransacoesRepositorio()
-  var transacoes = await repositorio.listaTransacoes()
+app.get('/transacoes', async (req, res) => {
+    mostraReq(req)
+    const repositorio = new TransacoesRepositorio()
+    const transacoes = await repositorio.listarTransacoes()
 
-  /*{
-    transacoes: [
-      {"valor": 10, "descricao": "bolo", categoria: "Despesa"}
-    ]
-  }*/
-  let saldo = 0
-  transacoes.transacoes.forEach((transacao) => {
-    if(transacao.categoria === "Despesa") {
-      saldo = saldo - transacao.valor
-    }
-    if (transacao.categoria === "Receita") {
-      saldo = saldo + transacao.valor
-    }
-  })
-  transacoes.saldo = saldo
+    let saldo = 0
+    transacoes.transacoes.forEach((transacao) => {
+        if (transacao.categoria === "Despesa") {
+            saldo = saldo - transacao.valor
+        }
+        if (transacao.categoria === "Receita") {
+            saldo = saldo + transacao.valor
+        }
+    })
 
-  res.send(transacoes)
+    transacoes.saldo = saldo
+
+    res.send(transacoes)
 })
 
 app.post('/transacoes', async (req, res) => {
-  const repositorio = new TransacoesRepositorio()
-  const transacao = req.body
-  await repositorio.criarTransacao(transacao)
-  res.status(201).send(transacao)
-}) 
+    mostraReq(req)
+    const repositorio = new TransacoesRepositorio()
+    const transacao = req.body
+    await repositorio.criarTransacao(transacao)
+    res.status(201).send(transacao)
+})
 
 app.listen(port, () => {
-  console.log(`Servidor ouvindo na porta ${port}`)
-});
-
-
-
+    console.log(`Servidor ouvindo na porta ${port}`)
+})
